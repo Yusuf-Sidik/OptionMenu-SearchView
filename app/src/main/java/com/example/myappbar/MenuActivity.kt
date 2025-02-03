6202be95b7ffe4f5d9a62e65f85d9fb90ef56417
yusuf <yusufnew060@gmail.com>
2025-02-03 11:48:41 +0700

    Menambahkan search view dan search bar

--- app/src/main/java/com/example/myappbar/MenuActivity.kt
+++ app/src/main/java/com/example/myappbar/MenuActivity.kt
@@ -1,20 +1,37 @@
 package com.example.myappbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myappbar.databinding.ActivityMainBinding
+import com.example.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchBar.inflateMenu(R.menu.option_menu)
+            searchBar.setOnMenuItemClickListener{ menuItem ->
+                // Handle menuItem click.
+                true
+            }
+            searchView
+                .editText
+                .setOnEditorActionListener{ textView, actionId, event ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
         }
     }
 }
